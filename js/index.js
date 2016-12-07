688915881a12f538814d34efbf0bf893d9dc242e
Tetiana <tetyana.fomscheva@mail.ru>
2016-12-07 21:57:39 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -13,13 +13,20 @@
 //function add(a,b){
 //    return a+b;
 //}
-function getAge (age){
-    if (age>18){
-        return 'Adult';
-    } else {
-        return 'Child'
-    }
-}
-var age = Number(prompt('Enter your name'));
-var result = getAge(age);
-document.write(result);
+//function getAge (age){
+ //   if (age>18){
+ //       return 'Adult';
+ //   } else {
+ //       return 'Child'
+ //   }
+//}
+//var age = Number(prompt('Enter your name'));
+//var result = getAge(age);
+//document.write(result);
+//∀x≥0,Math.sqrt(x)=x=the unique y≥0such that y2=x
+var a = Math.sqrt(0);
+var x = Math.sqrt(Number)
+if number>0;
+var y = Nan
+if y<0
+document.write(result)
